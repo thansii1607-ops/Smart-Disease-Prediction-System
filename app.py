2a724c0363354b01e2976e5c7c4b3bc0f2b28138
thansii1607-ops <thansii1607@gmail.com>
2026-06-17 13:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,37 @@
 import streamlit as st
+import streamlit as st
+
+# ===== LOGIN CODE =====
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+def login():
+    if username == "admin" and password == "1234":
+        st.session_state.logged_in = True
+    else:
+        st.error("Invalid Credentials")
+
+if not st.session_state.logged_in:
+    st.title("🔐 Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    st.button("Login", on_click=login)
+
+# ===== MAIN WEBSITE =====
+else:
+    st.title("🩺 Smart Disease Prediction System")
+
+    age = st.number_input("Age", 1, 120)
+
+    gender = st.selectbox(
+        "Gender",
+        ["Male", "Female"]
+    )
+
+    fever = st.selectbox("Fever", ["Yes", "No"])
+
+    if st.button("Predict"):
+        st.success("Prediction Result Here")
 
 st.set_page_config(page_title="Smart Disease Prediction System")
 
