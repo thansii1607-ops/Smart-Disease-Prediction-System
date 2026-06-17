2312fa59dbeeade070c5c3ca65075bd5541397a4
thansii1607-ops <thansii1607@gmail.com>
2026-06-17 16:20:52 +0530

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import json
-import os
-
-USER_FILE = "users.json"
 
 st.title("🔐 Login")
 
@@ -10,20 +7,12 @@ username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Login"):
+    with open("users.json", "r") as f:
+        users = json.load(f)
 
-    if not os.path.exists(USER_FILE):
-        st.error("No users found. Please register first.")
-
+    if username in users and users[username] == password:
+        st.session_state["logged_in"] = True
+        st.session_state["user"] = username
+        st.success("Login Successful ✅")
     else:
-        with open(USER_FILE, "r") as f:
-            users = json.load(f)
-
-        if username in users and users[username] == password:
-            st.session_state["logged_in"] = True
-            st.session_state["user"] = username
-
-            st.success(f"Welcome {username}! 🎉")
-            st.info("Go to the Home page from the sidebar.")
-
-        else:
-            st.error("Invalid Username or Password")
+        st.error("Invalid Username or Password")
